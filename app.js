d06f1447b7b455bbaf4d52addcee02d4e0ee5532
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2018-12-20 13:35:21 -0200

    add files

--- app.js
+++ app.js
@@ -92,6 +92,20 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+//DELETE ROUTE
+
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+    //redirect someore
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNING!");
 });

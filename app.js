a61562b2f62d1e5efff6dc7e955e835a092b79b5
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2018-12-20 03:58:50 -0200

    add files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var bodyParser  = require("body-parser"),
 express         = require("express"),
+methodOverride  = require("method-override"),
 mongoose        = require("mongoose"),
 app             = express();
 
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app",  { useNewUrlParser: tru
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -62,6 +64,30 @@ app.get("/blogs/:id", function(req, res) {
            res.redirect("/blogs");
        }else{
            res.render("show", {blog: foundBlog});
+       }
+    });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog: foundBlog});
+            
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    //Blog.findByIdAndUpdate(id, newData, callback)
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+       if(err){
+           res.redirect("/blogs");
+       }else{
+           res.redirect("/blogs/" + req.params.id);
        }
     });
 });

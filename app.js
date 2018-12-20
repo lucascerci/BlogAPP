1b2ade2113158f1f5897a99208cbe77cc824ccaa
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2018-12-20 02:21:04 -0200

    add files

--- app.js
+++ app.js
@@ -56,6 +56,16 @@ app.post("/blogs", function(req, res){
     });
 });
 
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       }else{
+           res.render("show", {blog: foundBlog});
+       }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNING!");
 });
